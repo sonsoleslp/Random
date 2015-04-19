b7163ce75d21184a204f28809db21f9943ff3620
Antonio Alférez <aj.alferez@alumnos.upm.es>
2015-04-19 10:27:01 +0200

    Añadiendo string predeterminado

--- random.js
+++ random.js
@@ -1,7 +1,9 @@
 var numero = Math.random();
 
+var str=" MAYOR que 0,5";
+
 if(numero<=0.5){
-	console.log ("\n" +numero+ ' menor que 0,5 \n');
-}else{
-	console.log(numero+" mayor que 0,5");
+	str=" MENOR que 0,5";
 }
+
+console.log("\n" +numero+ str+"\n");
